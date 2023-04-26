31e807fcfbf4f17bf426681b246b953fdf0b752b
izida kubekova <isedak81@gmail.com>
2023-04-26 09:54:29 +0600

    Создан сервер

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+class App {
+    private app: Express
+
+    constructor () {
+        this.app = express();
+        this.app.use(express.urlencoded({extended: true}));
+        this.app.use(express.static('public'));
+        this.app.use(express.json());
+        this.app.use(cors());
+    }
+
+    public init = async (): Promise<void> => {
+    try {
+        this.app.listen(process.env.APP_PORT, () => {
+            console.log(`Server is running on port ${process.env.APP_PORT}`);
+        })
+    } catch (error) {
+        const err = error as Error;
+        console.log(`Server running error: ${err}`);
+    }
+    }
+}
+
+const app = new App();
+
+app.init()
+.then(() => {
+    console.log('Server is OK');
+}).catch(() => {
+    console.log('Server is NOT OK');
+})
