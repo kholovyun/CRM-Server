ba687a3a24f3bc6239bc251156b8e27fc4e2cb52
kholov yunus <kholov_yun@gmail.com>
2023-06-13 16:38:32 +0600

    [DP-109] Сделал тесты на регистрацию

--- src/__tests__/userApi.test.ts
+++ src/__tests__/userApi.test.ts
@@ -1,35 +1,99 @@
-import request from "supertest";
-import express, { Express } from "express";
-import IUserCreateDto from "../interfaces/IUser/IUserCreateDto";
-import { UsersController } from "../controllers/usersController";
+import { StatusCodes } from "http-status-codes";
+import { User } from "../models/User";
+import { UsersDb } from "../repository/subDb/usersDb";
 import { ERoles } from "../enums/ERoles";
-import { PostgresDB } from "../repository/postgresDb";
+import { EErrorMessages } from "../enums/EErrorMessages";
 
 
-describe("/users registration route", () => {
-    let app: Express;
+jest.mock("../models/User");
+jest.mock("../models/Doctor");
 
-    const db = PostgresDB;
-    beforeAll(() => {
-        app = express();
-        app.use(express.json());
-        app.use("/users", new UsersController().getRouter());
+describe("UsersController", () => {
+    describe("register", () => {
+        test("should register a new user", async () => {
+            const userDto = {
+                role: ERoles.ADMIN,
+                email: "test@example.com",
+                phone: "+77086666666",
+                name: "John",
+                surname: "Doe",
+                patronim: "Smith",
+            };
+
+            const mockedCreate = jest.fn().mockResolvedValue({
+                id: "1",
+                role: ERoles.ADMIN,
+                email: "test@example.com",
+                phone: "123456789",
+                name: "John",
+                surname: "Doe",
+                patronim: "Smith",
+            });
+
+            User.findOne = jest.fn().mockResolvedValue(null);
+            User.create = mockedCreate;
+            const db = new UsersDb ();
+            const result = await db.register(userDto);
+            expect(User.findOne).toHaveBeenCalledWith({
+                where: { email: userDto.email },
+            });
+            expect(User.create).toHaveBeenCalledWith({
+                ...userDto,
+                password: "123",
+            });
+            expect(result).toEqual({
+                status: StatusCodes.CREATED,
+                result: {
+                    id: "1",
+                    role: ERoles.ADMIN,
+                    email: "test@example.com",
+                    phone: "123456789",
+                    name: "John",
+                    surname: "Doe",
+                    patronim: "Smith",
+                },
+            });
+        });
     });
-
-    it("should register a new user", async () => {
-        const newUser: IUserCreateDto = {
-            "role": ERoles.DOCTOR,
-            "email": "doct.yuncc@gmail.com",
-            "phone": "877777777",
-            "name": "Doc",
-            "surname": "Kholov",
-            "patronim": "string"
+    test("should throw an error if user already exists", async () => {
+        const userDto = {
+            role: ERoles.ADMIN,
+            email: "test@example.com",
+            phone: "+77086666666",
+            name: "John",
+            surname: "Doe",
+            patronim: "Smith",
         };
-        const response = await request(app)
-            .post("/users")
-            .send(newUser)
-            .expect(201);
-
-        expect(response.body).toEqual(expect.objectContaining(newUser));
+  
+        const mockedFindOne = jest.fn().mockResolvedValue(userDto);
+        User.findOne = mockedFindOne;
+        const db = new UsersDb();
+        const result = await db.register(userDto);
+        expect(result.status).toEqual(StatusCodes.BAD_REQUEST);
+        //@ts-ignore
+        expect(result.result.message).toEqual(EErrorMessages.USER_ALREADY_EXISTS);
+    });
+  
+    test("should throw an error if email format is incorrect", async () => {
+        const userDto = {
+            role: ERoles.DOCTOR,
+            email: "281u1hjnfal",
+            phone: "+77086666666",
+            name: "John",
+            surname: "Doe",
+            patronim: "Smith",
+        };
+        const db = new UsersDb();
+        User.findOne = jest.fn().mockResolvedValue(null);
+        //@ts-ignore
+        const result = await db.register(userDto);
+        expect(result.status).toEqual(StatusCodes.BAD_REQUEST);
+        //@ts-ignore
+        expect(result.result.status).toEqual("error");
+        //@ts-ignore
+        expect(result.result.message).toEqual(EErrorMessages.WRONG_MAIL_FORMAT);
     });
 });
+
+
+
