056c1a27bec68aeac5c74d6d06914f451f20e6ab
Alen <bolatovalen@gmail.com>
2023-06-26 16:54:48 +0600

    [DP-149] добавил контроллер для подписок

--- src/controllers/subscriptionsController.ts
+++ src/controllers/subscriptionsController.ts
@@ -1,3 +1,31 @@
-export class subscriptionsController {
+import {subscriptionsDb, SubscriptionsDb} from "../repository/subDb/subscriptionsDb";
+import express, {Request, Response, Router} from "express";
+import IRequestWithTokenData from "../interfaces/IRequestWithTokenData";
+import IResponse from "../interfaces/IResponse";
+import IError from "../interfaces/IError";
+import ISubscriptionGetDto from "../interfaces/ISubscription/ISubscriptionGetDto";
+import {permission} from "../middleware/permission";
+import {ERoles} from "../enums/ERoles";
+
+export class SubscriptionsController {
+    private repository: SubscriptionsDb;
+    private readonly router: Router;
+
+    constructor() {
+        this.repository = subscriptionsDb;
+        this.router = express.Router();
+        this.router.patch("/:id", permission([ERoles.DOCTOR, ERoles.PARENT]), this.renewSubscription)
+    }
+
+    public getRouter = (): Router => {
+        return this.router;
+    };
+
+    private renewSubscription = async (expressReq: Request, res: Response): Promise<void> => {
+        const req = expressReq as IRequestWithTokenData;
+        const user = req.dataFromToken as { id: string, email: string, role: string };
+        const response: IResponse<ISubscriptionGetDto | IError> = await this.repository.renewSubscription(user.id, req.params.id);
+        res.status(response.status).send(response.result);
+    };
 
 }
--- src/repository/subDb/subscriptionsDb.ts
+++ src/repository/subDb/subscriptionsDb.ts
@@ -4,12 +4,21 @@ import IResponse from "../../interfaces/IResponse";
 import IError from "../../interfaces/IError";
 import ISubscriptionGetDto from "../../interfaces/ISubscription/ISubscriptionGetDto";
 import {User} from "../../models/User";
+import {EErrorMessages} from "../../enums/EErrorMessages";
+import {Parent} from "../../models/Parent";
 
 export class SubscriptionsDb {
     public renewSubscription = async (userId: string, parentId: string):Promise<IResponse<ISubscriptionGetDto | IError>> => {
         try {
             const foundUser = await User.findByPk(userId);
+            if (!foundUser || foundUser.isBlocked) throw new Error(EErrorMessages.NO_ACCESS);
+            // User.find
+            // if (!foundParent) throw new Error(EErrorMessages.PARENT_NOT_FOUND);
 
+            // const isParentBlocked = await User.findByPk(foundParent?.userId)
+
+
+            // foundParent.
             return {
                 status: StatusCodes.OK,
                 result: undefined
